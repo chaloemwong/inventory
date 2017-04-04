52f410cea62c3545c87d54305750b91503d6b2bf
chaloemwong@gmail.com <chaloemwong@gmail.com>
2017-04-04 15:33:25 +0700

    add solution

---
+++ Core.IData/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.IData
+{
+    public interface IRepository<TEntity, in TKey> where TEntity : class
+    {
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        TEntity Get(TKey id);
+        IQueryable<TEntity> GetAll();
+    }
+}
